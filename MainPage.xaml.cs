103d06e1cb607cfdad6854d4ef52b57459a83e1c
ilyaDavidov <ilya.davidov63@mail.ru>
2023-02-08 19:05:27 +0200

    qwe

---
+++ MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using Microsoft.Maui.Graphics.Text;
+
+namespace MauiApp19;
+
+public partial class MainPage : ContentPage
+{
+	int number = 0;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		CancellationToken c = new CancellationToken();
+		Snackbar s = new Snackbar();
+		SnackbarOptions SsnackbarOptions = new SnackbarOptions()
+		{
+			BackgroundColor = Color.FromRgb(255, 0, 0),
+            TextColor = Color.FromRgb(255, 255, 0)
+        };
+		s = (Snackbar)Snackbar.Make("OWO", async () => await DisplayAlert("Welcom", "be-bei see you", "Okey"), "UWU", TimeSpan.FromSeconds(10),SsnackbarOptions);
+		Snackbar q = new Snackbar();
+        SnackbarOptions QsnackbarOptions = new SnackbarOptions()
+        {
+            BackgroundColor = Color.FromRgb(0, 0, 0),
+            TextColor = Color.FromRgb(0, 0, 255)
+        };
+        q = (Snackbar)Snackbar.Make("Quit", async () => await DisplayAlert("Welcom xenos", "Contingation see you", "Accept"), "Sure", TimeSpan.FromSeconds(10), QsnackbarOptions);
+        Snackbar es = new Snackbar();
+        SnackbarOptions EssnackbarOptions = new SnackbarOptions()
+        {
+            BackgroundColor = Color.FromRgb(10, 10, 10),
+            TextColor = Color.FromRgb(0, 0, 255)
+        };
+        es = (Snackbar)Snackbar.Make("Help", async () => await DisplayAlert("Buy Skyrim", "Buy Skyrim", "Buy"), "Yes", TimeSpan.FromSeconds(10), EssnackbarOptions);
+		switch (number)
+		{
+			case 0:
+				s.Show();
+				number++;
+				break;
+			case 1:
+				q.Show();
+				number++;
+				break;
+			case 2:
+				es.Show();
+				number = 0;
+				break;
+		}
+	}
+}
+
---
+++ obj/Debug/net7.0-android/android/src/crc646e5607b5257578c1/MainActivity.java
@@ -0,0 +1,48 @@
+package crc646e5607b5257578c1;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MauiApp19.MainActivity, MauiApp19", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MauiApp19.MainActivity, MauiApp19", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MauiApp19.MainActivity, MauiApp19", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
